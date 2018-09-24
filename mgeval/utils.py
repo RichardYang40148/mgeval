d1c56d219590545feb17a6e5a23d6552aabd7c42
Richard Yang <richard40148@gatech.edu>
2018-09-24 16:08:03 -0400

    update utilities for demo

---
+++ mgeval/utils.py
@@ -0,0 +1,55 @@
+import pretty_midi
+import numpy as np
+import sys
+import os
+import midi
+import glob
+import math
+import sklearn
+from scipy import stats, integrate
+import metrics as mt
+
+
+# Calculate overlap between the two PDF
+def overlap(A, B):
+    pdf_A = stats.gaussian_kde(A)
+    pdf_B = stats.gaussian_kde(B)
+    d_area = lambda x: min(pdf_A(x), pdf_B(x))
+    return integrate.quad(d_area, np.min((np.min(A), np.min(B))), np.max((np.max(A), np.max(B))))[0]
+
+
+# Calculate KL distance between the two PDF
+def _KL(A, B, num_sample=1000):
+    pdf_A = stats.gaussian_kde(A)
+    pdf_B = stats.gaussian_kde(B)
+    sample_A = np.linspace(np.min(A), np.max(A), num_sample)
+    sample_B = np.linspace(np.min(B), np.max(B), num_sample)
+    return stats.entropy(pdf_A(sample_A), pdf_B(sample_B))
+
+
+def _cdist(A, B, mode='None', normalize=0):
+    c_dist = np.zeros(len(B))
+    for i in range(0, len(B)):
+        if mode == 'None':
+            c_dist[i] = np.linalg.norm(A - B[i])
+        elif mode == 'EMD':
+            if normalize == 1:
+                A_ = sklearn.preprocessing.normalize(A.reshape(1, -1), norm='l1')[0]
+                B_ = sklearn.preprocessing.normalize(B[i].reshape(1, -1), norm='l1')[0]
+            else:
+                A_ = A.reshape(1, -1)[0]
+                B_ = B[i].reshape(1, -1)[0]
+
+            c_dist[i] = stats.wasserstein_distance(A_, B_)
+
+        elif mode == 'KL':
+            if normalize == 1:
+                A_ = sklearn.preprocessing.normalize(A.reshape(1, -1), norm='l1')[0]
+                B_ = sklearn.preprocessing.normalize(B[i].reshape(1, -1), norm='l1')[0]
+            else:
+                A_ = A.reshape(1, -1)[0]
+                B_ = B[i].reshape(1, -1)[0]
+
+            B_[B_ == 0] = 0.00000001
+            c_dist[i] = stats.entropy(A_, B_)
+    return c_dist
