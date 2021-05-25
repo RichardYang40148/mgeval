4bb9e16095b78c36d90e6e48f675527f102af36b
ghalestrilo <ghalestrilo@gmail.com>
2021-05-25 11:36:46 -0300

    🐛 fix duplicated result bug

--- __main__.py
+++ __main__.py
@@ -3,9 +3,11 @@ import json
 from argparse import ArgumentParser
 import midi
 import glob
+import copy
 import os
 import numpy as np
 import pretty_midi
+from pprint import pprint
 import pickle
 # import seaborn as sns
 # import matplotlib.pyplot as plt
@@ -21,18 +23,11 @@ parser.add_argument('--outfile', required=True, type=str,
                     help='File (pickle) where the analysis will be stored')
 args = parser.parse_args()
 
-# set1 = glob.glob('data/output/*')
-# print('Starting analysis of {} against {}'.format(args.set1dir, args.set2dir))
-
-# set1 = glob.glob(os.path.join(args.set1dir, '*'))
 set1 = glob.glob(os.path.join(args.set1dir, '*'))
 set2 = glob.glob(os.path.join(args.set2dir, '*'))
 
-# print('set1 has {} elements'.format(len(set1)))
-# print('set2 has {} elements'.format(len(set2)))
 
 num_samples = min(len(set2), len(set1))
-
 evalset = { 
             'total_used_pitch': np.zeros((num_samples, 1))
           # , 'total_used_note': np.zeros((num_samples, 1))
@@ -40,13 +35,13 @@ evalset = {
           # , 'avg_pitch_shift': np.zeros((num_samples, 1))
           , 'avg_IOI': np.zeros((num_samples, 1))
           # , 'total_used_note': np.zeros((num_samples, 1))
-          # , 'bar_used_pitch': np.zeros((num_samples, 1))
+          # , 'bar_used_pitch': np.zeros((num_samples, 1, 1))
           # , 'bar_used_note': np.zeros((num_samples, 1))
           , 'total_pitch_class_histogram': np.zeros((num_samples, 12))
           # , 'bar_pitch_class_histogram': np.zeros((num_samples, 1))
+          # , 'note_length_hist': np.zeros((num_samples, 12))
           , 'pitch_class_transition_matrix': np.zeros((num_samples, 12, 12))
-          # , 'note_length_hist': np.zeros((num_samples, 1))
-          , 'note_length_transition_matrix': np.zeros((num_samples, 12, 12))
+          # , 'note_length_transition_matrix': np.zeros((num_samples, 12, 12))
           }
 metrics_list = evalset.keys()
 
@@ -58,62 +53,58 @@ single_arg_metrics = (
     , 'pitch_range'
     ])
 
-set1_eval = evalset.copy()
+# Process First Dataset metrics
+set1_eval = copy.deepcopy(evalset)
+# print('set1')
+# pprint(set1[:num_samples])
 for i in range(0, num_samples):
     feature = core.extract_feature(set1[i])
     for metric in metrics_list:
-        # print('i = {}, metric = {}, len(metric) = {}'.format(i, metric, len(set1_eval[metric])))
         evaluator = getattr(core.metrics(), metric)
         if metric in single_arg_metrics:
             set1_eval[metric][i] = evaluator(feature)
         else:
             set1_eval[metric][i] = evaluator(feature, 0)
+# print('metric_set')
+# pprint(set1_eval)
 
-set2_eval = evalset.copy()
+# Process First Dataset metrics
+set2_eval = copy.deepcopy(evalset)
+# print('set2')
+# pprint(set2[:num_samples])
 for i in range(0, num_samples):
     feature = core.extract_feature(set2[i])
     for metric in metrics_list:
-        # print('i = {}, metric = {}, len(metric) = {}'.format(i, metric, len(set1_eval[metric])))
         evaluator = getattr(core.metrics(), metric)
         if metric in single_arg_metrics:
             set2_eval[metric][i] = evaluator(feature)
         else:
             set2_eval[metric][i] = evaluator(feature, 0)
-
-for metric in metrics_list:
-    print metric + ':'
-    # print '------------------------'
-    # print ' demo_set'
-    # print '  mean: ', np.mean(set1_eval[metric], axis=0)
-    # print '  std: ', np.std(set1_eval[metric], axis=0)
-
-    # print '------------------------'
-    # print ' demo_set'
-    # print '  mean: ', np.mean(set2_eval[metric], axis=0)
-    # print '  std: ', np.std(set2_eval[metric], axis=0)
-
+# print('metric_set')
+# pprint(set2_eval)
 
 loo = LeaveOneOut()
 loo.get_n_splits(np.arange(num_samples))
 set1_intra = np.zeros((num_samples, len(metrics_list), num_samples - 1))
 set2_intra = np.zeros((num_samples, len(metrics_list), num_samples - 1))
-for i in range(len(metrics_list)):
+
+
+# Calculate Intra-set Metrics
+for i, metric in enumerate(metrics_list):
     for train_index, test_index in loo.split(np.arange(num_samples)):
         set1_intra[test_index[0]][i] = utils.c_dist(
             set1_eval[metrics_list[i]][test_index], set1_eval[metrics_list[i]][train_index])
         set2_intra[test_index[0]][i] = utils.c_dist(
             set2_eval[metrics_list[i]][test_index], set2_eval[metrics_list[i]][train_index])
 
-print(set1_intra)
-
 loo = LeaveOneOut()
 loo.get_n_splits(np.arange(num_samples))
 sets_inter = np.zeros((num_samples, len(metrics_list), num_samples))
 
-for i in range(len(metrics_list)):
+# Calculate Inter-set Metrics
+for i, metric in enumerate(metrics_list):
     for train_index, test_index in loo.split(np.arange(num_samples)):
-        sets_inter[test_index[0]][i] = utils.c_dist(
-            set1_eval[metrics_list[i]][test_index], set2_eval[metrics_list[i]])
+        sets_inter[test_index[0]][i] = utils.c_dist(set1_eval[metric][test_index], set2_eval[metric])
 
 
 plot_set1_intra = np.transpose(
@@ -126,44 +117,27 @@ plot_sets_inter = np.transpose(
 
 output = {}
 for i, metric in enumerate(metrics_list):
-    # print('')
-    # print('plot_set1_intra[{}]'.format(i))
-    # print(plot_set1_intra[i])
-    # print('plot_set2_intra[{}]'.format(i))
-    # print(plot_set2_intra[i])
-    # print('plot_sets_inter[{}]'.format(i))
-    # print(plot_sets_inter[i])
+    # print('calculating kl of: {}'.format(metric))
 
-    # print metrics_list[i] + ':'
-    # print '------------------------'
-    # print ' demo_set1'
-    # print '  Kullback–Leibler divergence:', utils.kl_dist(plot_set1_intra[i], plot_sets_inter[i])
-    # print '  Overlap area:', utils.overlap_area(plot_set1_intra[i], plot_sets_inter[i])
-
-    # print ' demo_set2'
-    # print '  Kullback–Leibler divergence:', utils.kl_dist(plot_set2_intra[i], plot_sets_inter[i])
-    # print '  Overlap area:', utils.overlap_area(plot_set2_intra[i], plot_sets_inter[i])
-
-    # FIXME: Remove this
-    # plot_set1_intra[i] = np.nan_to_num(plot_set1_intra[i]) + 1
-    # plot_set2_intra[i] = np.nan_to_num(plot_set2_intra[i]) + 1
-    # plot_sets_inter[i] = np.nan_to_num(plot_sets_inter[i]) + 1
+    mean = np.mean(set1_eval[metric], axis=0).tolist()
+    std = np.std(set1_eval[metric], axis=0).tolist()
 
     kl1 = utils.kl_dist(plot_set1_intra[i], plot_sets_inter[i])
     ol1 = utils.overlap_area(plot_set1_intra[i], plot_sets_inter[i])
     kl2 = utils.kl_dist(plot_set2_intra[i], plot_sets_inter[i])
     ol2 = utils.overlap_area(plot_set2_intra[i], plot_sets_inter[i])
-    output[metric] = [kl1, ol1, kl2, ol2]
+
+    print(kl1)
+    print(kl2)
+    output[metric] = [mean, std, kl1, ol1, kl2, ol2]
 
 
 # Save output
 if os.path.exists(args.outfile):
     os.remove(args.outfile)
-with open(args.outfile, 'w') as f:
-    # pickle.dump(output, f)
-    json.dump(output, f)
+
+output_file = open(args.outfile, 'w')
+json.dump(output, output_file)
+output_file.close()
 
 print('Saved output to file: ' + args.outfile)
-print('output: ')
-
-print(output)
